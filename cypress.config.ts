c930f459dcb47f5cbe5369177f58f326b92e8773
mrcfps <mrc@powerformer.com>
2025-02-05 17:11:24 +0800

    test: configure end-to-end tests in CI

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,11 +2,11 @@ import { defineConfig } from 'cypress';
 
 export default defineConfig({
   e2e: {
+    defaultBrowser: 'chrome',
     viewportWidth: 1920,
     viewportHeight: 1080,
     baseUrl: 'http://localhost:5173',
     env: {
-      apiUrl: 'http://localhost:5800',
       databaseUrl: 'postgresql://refly:test@localhost:5432/refly',
     },
   },
