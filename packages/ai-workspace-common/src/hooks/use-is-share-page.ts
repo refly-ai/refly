7b7d687d84fac7cd9aa509d96b099393e6bee158
anthhub <anthhub@163.com>
2025-12-02 18:51:41 +0800

    fix: formart

--- packages/ai-workspace-common/src/hooks/use-is-share-page.ts
+++ packages/ai-workspace-common/src/hooks/use-is-share-page.ts
@@ -7,8 +7,7 @@ export const usePublicAccessPage = () => {
   const isArtifactGalleryPage = location?.pathname?.startsWith('/artifact-gallery') ?? false;
   const isUseCasesGalleryPage = location?.pathname?.startsWith('/use-cases-gallery') ?? false;
   const isAppPage = location?.pathname?.startsWith('/app/') ?? false;
-  const isWorkflowTemplatePage =
-    location?.pathname?.startsWith('/workflow-template/') ?? false;
+  const isWorkflowTemplatePage = location?.pathname?.startsWith('/workflow-template/') ?? false;
   const isLoginPage = (location?.pathname ?? '') === '/login';
   return (
     isPreviewPage ||
