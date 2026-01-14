7acc5b5f5ad2863b9ff5e8bed4587ac2b58c85b4
mashu <935902669@qq.com>
2026-01-14 22:39:04 +0800

    feat(perf): critical path prefetching & service worker code caching

--- packages/web-core/src/hooks/use-prefetch-workflow.ts
+++ packages/web-core/src/hooks/use-prefetch-workflow.ts
@@ -6,7 +6,10 @@ export const usePrefetchWorkflow = () => {
     // Dynamic import will trigger the browser to download the chunks
     // The /* webpackPrefetch: true */ comment is a hint to webpack/rspack
     // but standard dynamic import is enough for runtime discovery
-    import('../pages/workflow');
+    typeof requestIdleCallback !== 'undefined' &&
+      requestIdleCallback(() => {
+        import('../pages/workflow');
+      });
   }, []);
 
   return prefetch;
